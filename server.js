c56ba40e4d8657c8ea5061d9f784be4307a082a4
Colin Ritchey <colinritchey256@gmail.com>
2017-12-04 15:50:36 -0800

    connection to mLab

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+
+let db;
+let user = process.env.MONGODBUSER;
+let password = process.env.MONGODBPASSWORD;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-MongoClient.connect('link-to-mongodb', (err, database) => {
-  // ... start the server
-})
+MongoClient.connect(`mongodb://${user}:${password}@ds129796.mlab.com:29796/express-mongodb-sample`, (err, database) => {
+  if (err) return console.log(err)
+  db = database;
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  });
+
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
